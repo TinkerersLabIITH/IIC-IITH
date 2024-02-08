dc06399dcd7b2224e175ed6d9199a614ea63f67d
Suddhu-IITH2004 <suddhu040804@gmail.com>
2024-02-08 13:54:03 +0530

    Navbar

---
+++ components/header.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import "./header.css"
+
+
+function Header(){
+
+
+  const Hamburger = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="52"
+      height="24"
+      viewBox="0 0 52 24"
+    >
+      <g id="Group_9" data-name="Group 9" transform="translate(-294 -47)">
+        <rect
+          id="Rectangle_3"
+          data-name="Rectangle 3"
+          width="42"
+          height="4"
+          rx="2"
+          transform="translate(304 47)"
+          fill="#574c4c"
+        />
+        <rect
+          id="Rectangle_5"
+          data-name="Rectangle 5"
+          width="42"
+          height="4"
+          rx="2"
+          transform="translate(304 67)"
+          fill="#574c4c"
+        />
+        <rect
+          id="Rectangle_4"
+          data-name="Rectangle 4"
+          width="52"
+          height="4"
+          rx="2"
+          transform="translate(294 57)"
+          fill="#574c4c"
+        />
+      </g>
+    </svg>
+  );
+
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img className="logoImage" src="\components\logo.png" alt="Logo" />
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul>
+            <li>
+              Home
+            </li>
+            <li>
+              Blogs
+            </li>
+            <li>
+              Projects
+            </li>
+            <li>
+              About
+            </li>
+            <li>
+              Contact
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+
+
+}
+
+export default Header
