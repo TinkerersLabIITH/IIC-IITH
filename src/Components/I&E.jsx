57bc1f3ccda543a2d27c6338eb79f97ac9ca95db
anikettsatpute <cs21btech11056@iith.ac.in>
2024-02-09 21:33:20 +0530

    I&E and Major&Functions part created

---
+++ src/Components/I&E.jsx
@@ -0,0 +1,13 @@
+// creating a component and exporting it
+import React from "react";
+import { useState } from "react";
+
+function IandE() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>{/* /two columns one with image other with Text/ */}</div>
+    </>
+  );
+}
+export default IandE;
